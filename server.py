c283f02624c8ca90a43677f336e8c2d92c839b3e
Mehmet Can Gün <mehmet-can-gun@hotmail.com>
2021-12-29 18:11:01 +0300

    Folder structure and pages are created.

---
+++ server.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from src.routes import *
+
+app = Flask(__name__, template_folder="src/views")
+app.config.from_object('config')
+
+app.add_url_rule("/", view_func=AppController.home_page)
+
+if __name__ == "__main__":
+    app.run()

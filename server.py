f31301e459e4823446ea99d7312c8c16f0875ad0
Mehmet Can Gün <mehmet-can-gun@hotmail.com>
2022-01-01 00:59:43 +0300

    server merged previous server.py

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask
 
 from src.routes import *
@@ -15,16 +14,3 @@ if __name__ == "__main__":
     db.init_app(app)
     migrate.init_app(app, db)
     app.run()
-=======
-from flask import Flask
-from src.routes import *
-
-app = Flask(__name__, template_folder="src/views")
-app.config.from_object('config')
-
-app.add_url_rule("/", view_func=AppController.home_page)
-
-
-if __name__ == "__main__":
-    app.run()
->>>>>>> ad5c79b0a9f3785977c57549d4a36bbec76b3066

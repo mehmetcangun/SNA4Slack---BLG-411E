ad5c79b0a9f3785977c57549d4a36bbec76b3066
msalihyldz <muhammet_5628@hotmail.com>
2021-12-31 12:35:50 +0300

    preference page created

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ app = Flask(__name__, template_folder="src/views")
 app.config.from_object('config')
 
 app.add_url_rule("/", view_func=AppController.home_page)
+app.add_url_rule("/preference", view_func=AppController.preference_page)
 
 if __name__ == "__main__":
     app.run()
--- src/controllers/AppController.py
+++ src/controllers/AppController.py
@@ -3,3 +3,6 @@ from flask import render_template
 
 def home_page():
     return render_template("app.html")
+
+def preference_page():
+    return render_template("preference.html")

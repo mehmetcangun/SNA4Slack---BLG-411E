c285e8630b1b5b5734364d5dcd52ee6ccb0ab114
msalihyldz <muhammet_5628@hotmail.com>
2022-01-05 05:10:04 +0300

    zip extract and dynamic preference done

--- src/controllers/AppController.py
+++ src/controllers/AppController.py
@@ -2,6 +2,7 @@ from flask import render_template, request, session, redirect
 
 from .UserController import save_user, get_user_count, get_user_count_having_files
 from .SNAController import get_rate
+from .FileController import extract_file
 import random
 
 def home_page():
@@ -26,10 +27,12 @@ def home_page():
     return render_template("app.html")
 
 def preference_page():
-
+    #extract_file('asd123')
+    metric_labels, metrics_rate = get_rate("metric")
+    layout_labels, layouts_rate = get_rate("layout")
     colors = ["#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
-             for i in range(5)]
-    return render_template("preference.html", colors=colors)
+             for i in range(max(len(metric_labels), len(layout_labels)))]
+    return render_template("preference.html", colors=colors, layout_data=[layout_labels, layouts_rate], metric_data=[metric_labels, metrics_rate])
 
 def evaluate_metric_layout():
     metric = request.form['metric']
